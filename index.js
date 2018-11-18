56d4d15f6a79550b2e7b016b5d87da961fea0047
unknown <tadej.rola@gmail.com>
2018-11-18 18:26:48 +0200

    initial

---
+++ index.js
@@ -0,0 +1,56 @@
+navigator.webkitGetUserMedia({ video: false, audio: true }, function (stream) {
+    // var Peer = require("simple-peer");
+
+    var peer = new SimplePeer({
+        initiator: location.hash === "#init",
+        trickle: false,
+        reconnectTimer: 100,
+        iceTransportPolicy: 'relay',
+        config: {
+
+            iceServers: [
+                {
+                    urls: "stun:numb.viagenie.ca",
+                    username: "tadej.rola@gmail.com",
+                    credential: "test123"
+                },
+                {
+                    urls: "turn:numb.viagenie.ca",
+                    username: "tadej.rola@gmail.com",
+                    credential: "test123"
+                }
+            ]
+        },
+        stream: stream
+    });
+
+    peer.on("signal", function (data) {
+        document.getElementById("yourid").value = JSON.stringify(data);
+    })
+
+
+    document.getElementById("connect").addEventListener("click", function () {
+        var otherid = JSON.parse(document.getElementById("otherid").value);
+        peer.signal(otherid);
+    })
+
+    document.getElementById("send").addEventListener("click", function () {
+        var yourmessege = document.getElementById("yourmessege").value;
+        peer.send(yourmessege);
+    })
+
+    peer.on("data", function (data) {
+        document.getElementById("messeges").textContent += data + "\n";
+    })
+
+    peer.on("stream", function (stream) {
+        var audio = document.createElement("audio");
+        document.body.appendChild(audio);
+
+        audio.src = window.URL.createObjectURL(stream);
+        audio.play();
+    })
+
+}, function (err) {
+    console.error(err);
+})
